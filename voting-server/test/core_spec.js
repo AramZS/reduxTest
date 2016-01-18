0f94af55ad4c7f4a115447991421cb7096257b8a
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-01-17 21:49:31 -0500

    Setting up more core functions and spec.

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -15,6 +15,15 @@ describe('application logic', () => {
         entries: List.of('Trainspotting', '28 Days Later')
       }));
     });
+
+	it('converts to immutable', () => {
+	  const state = Map();
+	  const entries = ['Trainspotting', '28 Days Later'];
+	  const nextState = setEntries(state, entries);
+	  expect(nextState).to.equal(Map({
+	    entries: List.of('Trainspotting', '28 Days Later')
+	  }));
+	});
 
   });
 
