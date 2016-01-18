27fbfb9dd62cfa3449db7f16741e7e66fec09a68
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-01-18 18:41:11 -0500

    Setting up store and server

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,5 +1,5 @@
 import {setEntries, next, vote, INITIAL_STATE} from './core';
-
+import {Map, fromJS} from 'immutable';
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
   case 'SET_ENTRIES':
@@ -7,7 +7,9 @@ export default function reducer(state = INITIAL_STATE, action) {
   case 'NEXT':
     return next(state);
   case 'VOTE':
-    return vote(state, action.entry)
+  	// http://facebook.github.io/immutable-js/docs/#/Map/update
+	return state.update('vote',
+					  voteState => vote(voteState, action.entry));
   }
   return state;
 }
