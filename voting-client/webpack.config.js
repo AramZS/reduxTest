4cdd7d7104fc7446e5acf9645fc7396e6e63b64e
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-01-18 22:31:41 -0500

    Setting up babel config so it will work with hot loading.

--- voting-client/webpack.config.js
+++ voting-client/webpack.config.js
@@ -3,13 +3,20 @@ module.exports = {
 	entry: [
 		'webpack-dev-server/client?http://localhost:8080',
 	    'webpack/hot/only-dev-server',
-	    './src/index.js'
+	    './src/index.jsx'
 	],
   	module: {
 	  loaders: [{
 		  test: /\.jsx?$/,
 	      exclude: /node_modules/,
-	      loader: 'react-hot!babel'
+	      loader: 'babel',
+		  query: {
+			  presets: [
+				  require.resolve('babel-preset-es2015'),
+				  require.resolve('babel-preset-react')
+			  ]
+		  },
+		  plugins: ["react-hot"]
 	  }]
 	},
 	resolve: {
