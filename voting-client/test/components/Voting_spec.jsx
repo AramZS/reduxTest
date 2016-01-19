66cd93f60268a258315655c5e3b36b5493d11364
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-01-18 23:30:54 -0500

    Getting test working via babel presets in babelrc

--- voting-client/test/components/Voting_spec.jsx
+++ voting-client/test/components/Voting_spec.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Voting from '../../src/components/Voting';
 //var ReactTestUtils = require('react-addons-test-utils');
-import {renderIntoDocument} from 'react-addons-test-utils';
+import {expect} from 'chai';
+import {renderIntoDocument, scryRenderedDOMComponentsWithTag} from 'react-addons-test-utils';
 
 describe('Voting', () => {
 
@@ -9,6 +10,11 @@ describe('Voting', () => {
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting", "28 Days Later"]} />
     );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+
+    expect(buttons.length).to.equal(2);
+    expect(buttons[0].textContent).to.equal('Trainspotting');
+    expect(buttons[1].textContent).to.equal('28 Days Later');
   });
 
 });
